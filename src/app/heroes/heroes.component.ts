8682c596b342819863121056df0b967ebb3fa816
Andro Ranogajec <ranogaet@gmail.com>
2021-09-15 16:44:01 +0300

    select a hero conditionally include or excude a block of HTML

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  selectedHero?: Hero = {
     id: 1,
     name: 'Windstorm',
   };
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() {}
 
   ngOnInit(): void {}
+
+  heroes = HEROES;
 }
